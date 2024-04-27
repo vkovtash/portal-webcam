c34d7f559b23e15473c1f88e42440e9ea9ea4ad4
Vlad Kovtash <vlad@kovtash.com>
2024-04-26 20:50:17 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const urlParams = new URLSearchParams(window.location.search);
+const videoDeviceId = urlParams.get("deviceId");
+const audioDeviceId = urlParams.get("audioDeviceId");
+
+function startVideo() {
+  const constraints = {
+    video: { width: 1920, height: 1080 },
+    audio: true,
+  };
+  
+  constraints.audio.echoCancellation = false
+  constraints.audio.autoGainControl = false
+  constraints.audio.googAutoGainControl = false
+  constraints.audio.noiseSuppression = true
+
+  console.log({ constraints });
+  
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      document.querySelector("video").srcObject = stream;
+    });
+}
+
+function enterFullscreen() {
+  const element = document.documentElement;
+
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  }
+}
+
+startVideo();
